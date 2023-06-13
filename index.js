e8f8ee9b5867051ffbcd7616e828461214451610
Albright <albright.muchori@student.moringaschool.com>
2023-06-14 00:35:38 +0300

    conditional state

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+// Write your code here!
+if (distance <=400){
+return "This one is on me!";
+}
+else if(distance >2000 && distance <2500){
+return "I will gladly take your thirty bucks.";
+}
+else if(distance >2500){
+return "No can do.";
+}
+}
+let city = 'NYC'
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+let tipAmount = '08';
+function switchOnCharmFromTip(tipAmount){
+
+  switch (tipAmount) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+   
+ 
+ }
+  
   // Write your code here!
-}
